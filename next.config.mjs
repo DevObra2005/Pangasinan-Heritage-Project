c385741f68e1204a13415f33262cd3c268925797
Mark Joemarie Obra <dev.obra2005@gmail.com>
2026-08-30 12:45:13 +0800

    Configure static export and GitHub Actions deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Pangasinan-Heritage-Project',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
